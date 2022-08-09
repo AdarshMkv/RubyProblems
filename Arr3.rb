39546347a1a212ad77583c63743d7132fda89dd5
Adarsh Mukundan <adarshmkv76@gmail.com>
2022-08-09 11:31:19 +0530

    Array Problem 3 updated with comment

--- Arr3.rb
+++ Arr3.rb
@@ -1,4 +1,11 @@
-
+=begin
+Take 20 integer inputs from user and print the following:
+number of positive numbers
+number of negative numbers
+number of odd numbers
+number of even numbers
+number of 0s.
+=end
 
 pos=[]
 neg=[]
